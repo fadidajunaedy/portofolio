8b31c659fb6858b01a86cd9fe9d3aa354f2e5565
Fadida Junaedy <fadidajunaedy@gmail.com>
2022-09-05 10:19:22 +0700

    "Add active link style"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import { AiOutlineAlignRight, AiOutlineClose } from 'react-icons/ai';
 
@@ -11,18 +11,25 @@ function Header() {
         setBcolor(!bcolor);
     };
 
+    const navLinkActive = ({ isActive }) => {
+        return {
+            color: isActive ? '#47C5D7': '#262642',
+            borderBottom: isActive ? '1.5px solid #262642': 'none'
+        }
+    }
+
 
     return (
         <>
         <header>
-            <Link to="/" className="logo">F.</Link>
+            <NavLink to="/" className="logo">F.</NavLink>
             <nav className={navbar ? "nav" : "nav nav-open"}>
                 <ul>
-                    <li><Link to="/" onClick={handleToggle}>Home</Link></li>
-                    <li><Link to="/about" onClick={handleToggle}>About</Link></li>
-                    <li><Link to="/skills" onClick={handleToggle}>Skills</Link></li>
-                    <li><Link to="/projects" onClick={handleToggle}>Projects</Link></li>
-                    <li><Link to="/contact" onClick={handleToggle}>Contact</Link></li>
+                    <li><NavLink style={navLinkActive} to="/" onClick={handleToggle}>Home</NavLink></li>
+                    <li><NavLink style={navLinkActive} to="/about" onClick={handleToggle}>About</NavLink></li>
+                    <li><NavLink style={navLinkActive} to="/skills" onClick={handleToggle}>Skills</NavLink></li>
+                    <li><NavLink style={navLinkActive} to="/projects" onClick={handleToggle}>Projects</NavLink></li>
+                    <li><NavLink style={navLinkActive} to="/contact" onClick={handleToggle}>Contact</NavLink></li>
                 </ul>
             </nav>
             <AiOutlineAlignRight color={bcolor ? "#262642" : "#FFFFFF"} className="toggle-navbar" onClick={handleToggle}/>
